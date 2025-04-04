26cc88ab1230166477dd2316d891122c8e8f8f46
Abbas0vv <ilhamabbasov46@gmail.com>
2025-04-04 13:31:13 +0400

    Student Task completed

--- ObjectLab/Program.cs
+++ ObjectLab/Program.cs
@@ -4,61 +4,40 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Which currency do you want? USD = 0/EUR = 1/RUB = 2/TRY = 3");
-            int usersCurrencyChoise = 0;
-            while (!int.TryParse(Console.ReadLine(), out usersCurrencyChoise))
+            Console.Write("Enter your Full Name : ");
+            string fullName = Console.ReadLine();
+
+            while (!Helper.CheckFullName(fullName))
             {
-                Console.WriteLine("Enter correct choise! 0/1/2/3");
+                Console.WriteLine("Enter your name correctly");
+                Console.Write("Enter your Full Name : ");
+                fullName = Console.ReadLine();
+            }
+            Console.WriteLine();
+            
+            Console.Write("Enter your age : ");
+            byte age = 0;
+            while (!byte.TryParse(Console.ReadLine(), out age))
+            {
+                Console.WriteLine("Enter your age correctly!");
+                Console.Write("Enter your age : ");
             }
             Console.WriteLine();
 
-            Console.WriteLine("Enter AZN value : ");
-            double valueForCurrencyTransfer = 0;
-            while (!double.TryParse(Console.ReadLine(), out valueForCurrencyTransfer))
+            Console.Write("Enter your Group Number : ");
+            string groupNumber = Console.ReadLine();
+            while (!Helper.CheckGroupNo(groupNumber))
             {
-                Console.WriteLine("Write correctly/Something went wrong.");
+                Console.WriteLine("Enter your Group Number correctly!");
+                Console.Write("Enter your Group Number : ");
+                groupNumber = Console.ReadLine();
             }
             Console.WriteLine();
 
+            Student student = new Student(fullName, groupNumber, age);
+            Console.WriteLine("Student added");
 
-            switch (usersCurrencyChoise)
-            {
-                case (int)Currency.USD:
-                    CalculateCurrency(Currency.USD, valueForCurrencyTransfer);
-                    break;
-                case (int)Currency.RUB:
-                    CalculateCurrency(Currency.RUB, valueForCurrencyTransfer);
-                    break;
-                case (int)Currency.EUR:
-                    CalculateCurrency(Currency.EUR, valueForCurrencyTransfer);
-                    break;
-                case (int)Currency.TRY:
-                    CalculateCurrency(Currency.TRY, valueForCurrencyTransfer);
-                    break;
-                default:
-                    break;
-            }
-
-        }
-        public static void CalculateCurrency(Currency currency, double azn)
-        {
-            switch (currency)
-            {
-                case Currency.EUR:
-                    Console.WriteLine($"{azn / 2}");
-                    break;
-                case Currency.USD:
-                    Console.WriteLine($"{azn / 1.7}");
-                    break;
-                case Currency.RUB:
-                    Console.WriteLine($"{azn / 0.02}");
-                    break;
-                case Currency.TRY:
-                    Console.WriteLine($"{azn / 0.05}");
-                    break;
-                default:
-                    break;
-            }
+            student.ShowInfo();
         }
     }
 }
---
+++ ObjectLab/Student.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectLab
+{
+    class Student
+    {
+        int _id = 1;
+        public Student(string fullName, string groupNo, byte age)
+        {
+            Id = _id;
+            FullName = fullName;
+            GroupNo = groupNo;
+            Age = age;
+        }
+
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string GroupNo { get; set; }
+        public byte Age { get; set; }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"{Id}. {FullName}, {Age} => {GroupNo}");
+        }
+    }
+}
