87e4afa16608c3d17ebe66d3a7a0e0ce40d402d2
Abbas0vv <ilhamabbasov46@gmail.com>
2025-04-09 13:44:47 +0400

    Planet/Country task completed

---
+++ ObjectLab/Models/Planet.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectLab.Models
+{
+    public class Planet
+    {
+        public string Name { get; set; }
+        public decimal Area { get; set; }
+        List<Country> Countries;
+
+        public Planet(string name, decimal area)
+        {
+            Name = name;
+            Area = area;
+        }
+
+        public void CreateAndAddCountry()
+        {
+            Console.WriteLine("Enter new country name");
+            string countryName = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("Enter new country area");
+            decimal area = 0;
+            while (!decimal.TryParse(Console.ReadLine(), out area))
+            {
+                Console.WriteLine("Enter correctly!");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Enter new country anthem");
+            string anthem = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("Enter new country region code");
+            int regionCode = 0;
+            while (!int.TryParse(Console.ReadLine(), out regionCode))
+            {
+                Console.WriteLine("Enter correctly");
+            }
+            Region? regionName;
+            switch (regionCode)
+            {
+                case 1:
+                    regionName = Region.Europe;
+                    break;
+                case 2:
+                    regionName = Region.Asia;
+                    break;
+                default:
+                    regionName = null;
+                    break;
+            }
+
+            Country country = new Country(countryName, area, anthem, regionName);
+            Console.WriteLine("Country created");
+            Console.WriteLine();
+            Countries.Add(country);
+            Console.WriteLine("Added");
+        }
+        public void UpdateCountry(int id, Country country)
+        {
+            for (int i = 0; i < Countries.Count; i++)
+            {
+                if (Countries[i].Id == id)
+                    Countries[i] = country;
+            }
+            Console.WriteLine("Updated");
+        }
+        public void RemoveCountry(int id, Country country)
+        {
+            Countries.RemoveAt(id);
+            Console.WriteLine("Country removed");
+        }
+        public List<Country> GetAllCountries()
+        {
+            Console.WriteLine("All Done!");
+            return Countries;
+        }
+        public List<Country> GetCountryByRegion(Region? region)
+        {
+            List<Country> foundednCountries = new List<Country>();
+            for (int i = 0; i < Countries.Count; i++)
+            {
+                if (Countries[i].CountryRegion == region)
+                    foundednCountries.Add(Countries[i]);
+            }
+            Console.WriteLine("All Done!");
+            return foundednCountries;
+        }
+    }
+}
--- ObjectLab/Program.cs
+++ ObjectLab/Program.cs
@@ -1,4 +1,5 @@
-﻿using ObjectLab.Models;
+﻿
+using ObjectLab.Models;
 
 namespace ObjectLab
 {
@@ -6,82 +7,119 @@ namespace ObjectLab
     {
         static void Main(string[] args)
         {
-            Library library = MakeNewLibrary();
-            Book book = MakeNewBook();
-            library.AddBook(MakeNewBook());
 
             while (true)
             {
-                Console.WriteLine("1.Add Book");
-                Console.WriteLine("2.Get book by Id");
-                Console.WriteLine("3.Remove book");
-                Console.WriteLine("4.Update book");
-                Console.WriteLine("5.Get all books");
-                Console.WriteLine("0.Quit");
+                bool flag = false;
+                Console.WriteLine("Enter 1 for :Sistemə giriş:");
+                Console.WriteLine("Enter 0 for :Çıxış:");
                 Console.WriteLine();
-                string userChoise = Console.ReadLine();
-
+                int userChoise = int.Parse(Console.ReadLine());
                 switch (userChoise)
                 {
-                    case "1":
-                        library.AddBook(MakeNewBook());
+                    case 1:
+                        string name = Console.ReadLine();
+                        decimal area = decimal.Parse(Console.ReadLine());
+                        Planet ourPlanet = new Planet(name, area);
+                        ourPlanet.CreateAndAddCountry();
+                        Console.Clear();
+                        while (!flag)
+                        {
+                            Console.WriteLine("Enter 1 for :Ölkə Yarat:");
+                            Console.WriteLine("Enter 2 for :Bütün ölkələri gör:");
+                            Console.WriteLine("Enter 3 for :Regiona görə ölkələri gör:");
+                            Console.WriteLine("Enter 4 for :Ölkəni yenilə:");
+                            Console.WriteLine("Enter 5 for :Ölkəni sil:");
+                            Console.WriteLine("Enter 0 for :Əvvəlki menuya qayıt:");
+                            Console.WriteLine();
+
+                            userChoise = int.Parse(Console.ReadLine());
+
+                            switch (userChoise)
+                            {
+                                case 1:
+                                    ourPlanet.CreateAndAddCountry();
+                                    break;
+                                case 2:
+                                    ourPlanet.GetAllCountries();
+                                    break;
+                                case 3:
+                                    Console.WriteLine("Enter region code");
+                                    int regionCode = 0;
+                                    while (!int.TryParse(Console.ReadLine(), out regionCode))
+                                    {
+                                        Console.WriteLine("Enter correctly");
+                                    }
+                                    Region? regionName;
+                                    switch (regionCode)
+                                    {
+                                        case 1:
+                                            regionName = Region.Europe;
+                                            break;
+                                        case 2:
+                                            regionName = Region.Asia;
+                                            break;
+                                        default:
+                                            regionName = null;
+                                            break;
+                                    }
+                                    ourPlanet.GetCountryByRegion(regionName);
+                                    break;
+                                case 4:
+                                    Console.WriteLine("Enter new country name");
+                                    string newCuntryName = Console.ReadLine();
+                                    Console.WriteLine();
+                                    Console.WriteLine("Enter new country area");
+                                    decimal newArea = 0;
+                                    while (!decimal.TryParse(Console.ReadLine(), out newArea))
+                                    {
+                                        Console.WriteLine("Enter correctly!");
+                                    }
+                                    Console.WriteLine();
+                                    Console.WriteLine("Enter new country anthem");
+                                    string newAnthem = Console.ReadLine();
+                                    Console.WriteLine();
+                                    Console.WriteLine("Enter new country region code");
+                                    int NewRegionCode = 0;
+                                    while (!int.TryParse(Console.ReadLine(), out NewRegionCode))
+                                    {
+                                        Console.WriteLine("Enter correctly");
+                                    }
+                                    Region? newRegionName;
+                                    switch (NewRegionCode)
+                                    {
+                                        case 1:
+                                            newRegionName = Region.Europe;
+                                            break;
+                                        case 2:
+                                            newRegionName = Region.Asia;
+                                            break;
+                                        default:
+                                            newRegionName = null;
+                                            break;
+                                    }
+                                    Country newCountry = new Country(newCuntryName, newArea, newAnthem, newRegionName);
+                                    Console.WriteLine();
+                                    Console.WriteLine("Enter id");
+                                    int id = int.Parse(Console.ReadLine());
+                                    ourPlanet.UpdateCountry(id, newCountry);
+                                    break;
+                                case 5:
+                                    break;
+                                case 0:
+                                    flag = true;
+                                    break;
+                                default:
+                                    break;
+                            }
+                        }
                         break;
-                    case "2":
-                        library.GetBookById(GetAndValidateId());
-                        break;
-                    case "3":
-                        library.RemoveBookById(GetAndValidateId());
-                        break;
-                    case "4":
-                        library.Update(GetAndValidateId(), MakeNewBook());
-                        break;
-                    case "5":
-                        library.GetAllBooks();
-                        break;
-                    case "0":
+                    case 0:
                         return;
                     default:
                         break;
                 }
-
             }
-        }
-        public static Book MakeNewBook()
-        {
-            Console.WriteLine("Make Book : ");
-            Console.Write("Enter book's name : ");
-            string bookName = Console.ReadLine();
-            Console.WriteLine("Enter author's name : ");
-            string authorName = Console.ReadLine();
-            Console.WriteLine("Enter book's price");
-            decimal price = 0;
-            while (!decimal.TryParse(Console.ReadLine(), out price))
-            {
-                Console.WriteLine("Enter correct price!");
-                Console.WriteLine("Enter book's price");
-            }
-            Book book = new Book(bookName, authorName, price);
-            Console.Clear();
-            return book;
-        }
-        public static Library MakeNewLibrary()
-        {
-            Console.WriteLine("Make Library : ");
-            Console.Write("Enter library name : ");
-            string libraryName = Console.ReadLine();
-            Library library = new Library(libraryName);
-            Console.Clear();
-            return library;
-        }
-        public static int GetAndValidateId()
-        {
-            int id = 0;
-            Console.Write("Enter Id : ");
-            while (!int.TryParse(Console.ReadLine(), out id))
-            {
-                Console.WriteLine("Enter Id Correctly!");
-            }
-            return id;
         }
     }
 }
