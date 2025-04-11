4046021110f5f847a56db0345d8375e52b5b4ddc
Abbas0vv <ilhamabbasov46@gmail.com>
2025-04-11 12:25:09 +0400

    User Task Completed

---
+++ ObjectLab/Models/Human.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectLab.Models
+{
+    public class Human
+    {
+        public Human(string name, string surname, byte age)
+        {
+            Id = _id;
+            Name = name;
+            Surname = surname;
+            Age = age;
+            _id++;
+        }
+        public Human(string name) { Name = name; }
+        public Human() { }
+
+        string _name;
+        static int _id;
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public byte Age { get; set; }
+
+        public void ShowFullData()
+        {
+            Console.WriteLine($"{Id}. {Name} {Surname} is {Age} years old.");
+        }
+        public string GetFullName()
+        {
+            return Name + " " + Surname;
+        }
+    }
+}
--- ObjectLab/Program.cs
+++ ObjectLab/Program.cs
@@ -7,119 +7,12 @@ namespace ObjectLab
     {
         static void Main(string[] args)
         {
+            Console.Write("Plz enter your username : ");
+            string userName = Console.ReadLine();
+            Console.Write("Plz enter your password : ");
+            string password = Console.ReadLine();
 
-            while (true)
-            {
-                bool flag = false;
-                Console.WriteLine("Enter 1 for :Sistemə giriş:");
-                Console.WriteLine("Enter 0 for :Çıxış:");
-                Console.WriteLine();
-                int userChoise = int.Parse(Console.ReadLine());
-                switch (userChoise)
-                {
-                    case 1:
-                        string name = Console.ReadLine();
-                        decimal area = decimal.Parse(Console.ReadLine());
-                        Planet ourPlanet = new Planet(name, area);
-                        ourPlanet.CreateAndAddCountry();
-                        Console.Clear();
-                        while (!flag)
-                        {
-                            Console.WriteLine("Enter 1 for :Ölkə Yarat:");
-                            Console.WriteLine("Enter 2 for :Bütün ölkələri gör:");
-                            Console.WriteLine("Enter 3 for :Regiona görə ölkələri gör:");
-                            Console.WriteLine("Enter 4 for :Ölkəni yenilə:");
-                            Console.WriteLine("Enter 5 for :Ölkəni sil:");
-                            Console.WriteLine("Enter 0 for :Əvvəlki menuya qayıt:");
-                            Console.WriteLine();
-
-                            userChoise = int.Parse(Console.ReadLine());
-
-                            switch (userChoise)
-                            {
-                                case 1:
-                                    ourPlanet.CreateAndAddCountry();
-                                    break;
-                                case 2:
-                                    ourPlanet.GetAllCountries();
-                                    break;
-                                case 3:
-                                    Console.WriteLine("Enter region code");
-                                    int regionCode = 0;
-                                    while (!int.TryParse(Console.ReadLine(), out regionCode))
-                                    {
-                                        Console.WriteLine("Enter correctly");
-                                    }
-                                    Region? regionName;
-                                    switch (regionCode)
-                                    {
-                                        case 1:
-                                            regionName = Region.Europe;
-                                            break;
-                                        case 2:
-                                            regionName = Region.Asia;
-                                            break;
-                                        default:
-                                            regionName = null;
-                                            break;
-                                    }
-                                    ourPlanet.GetCountryByRegion(regionName);
-                                    break;
-                                case 4:
-                                    Console.WriteLine("Enter new country name");
-                                    string newCuntryName = Console.ReadLine();
-                                    Console.WriteLine();
-                                    Console.WriteLine("Enter new country area");
-                                    decimal newArea = 0;
-                                    while (!decimal.TryParse(Console.ReadLine(), out newArea))
-                                    {
-                                        Console.WriteLine("Enter correctly!");
-                                    }
-                                    Console.WriteLine();
-                                    Console.WriteLine("Enter new country anthem");
-                                    string newAnthem = Console.ReadLine();
-                                    Console.WriteLine();
-                                    Console.WriteLine("Enter new country region code");
-                                    int NewRegionCode = 0;
-                                    while (!int.TryParse(Console.ReadLine(), out NewRegionCode))
-                                    {
-                                        Console.WriteLine("Enter correctly");
-                                    }
-                                    Region? newRegionName;
-                                    switch (NewRegionCode)
-                                    {
-                                        case 1:
-                                            newRegionName = Region.Europe;
-                                            break;
-                                        case 2:
-                                            newRegionName = Region.Asia;
-                                            break;
-                                        default:
-                                            newRegionName = null;
-                                            break;
-                                    }
-                                    Country newCountry = new Country(newCuntryName, newArea, newAnthem, newRegionName);
-                                    Console.WriteLine();
-                                    Console.WriteLine("Enter id");
-                                    int id = int.Parse(Console.ReadLine());
-                                    ourPlanet.UpdateCountry(id, newCountry);
-                                    break;
-                                case 5:
-                                    break;
-                                case 0:
-                                    flag = true;
-                                    break;
-                                default:
-                                    break;
-                            }
-                        }
-                        break;
-                    case 0:
-                        return;
-                    default:
-                        break;
-                }
-            }
+            User user = new User(userName, password);
         }
     }
 }
